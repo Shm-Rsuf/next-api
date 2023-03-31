fe5ccd158a5759425ab688f98b4552319828ed5a
shm_usuf <shmusuf@gmail.com>
2023-03-31 21:16:34 +0600

    front-end-part added

--- pages/players/index.js
+++ pages/players/index.js
@@ -6,6 +6,9 @@ const Players = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
 
+  const [playerName, setPlayerName] = useState("");
+  const [countryName, setCountryName] = useState("");
+
   useEffect(() => {
     const getPlayers = async () => {
       try {
@@ -26,6 +29,39 @@ const Players = () => {
     getPlayers();
   }, []);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    //creating a playerObj
+    const playerObj = {
+      id: Date.now(),
+      name: playerName,
+      country: countryName,
+    };
+
+    //post request
+    const res = await fetch("/api/players", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(playerObj),
+    });
+
+    const data = await res.json();
+
+    //if response is false
+    if (!res.ok) {
+      console.log("fail to fetch data");
+    }
+
+    //if response is true
+    if (res.ok) {
+      console.log("success", data);
+      // setPlayers([...players, data]);
+    }
+  };
+
   if (isLoading) {
     return (
       <div>
@@ -44,8 +80,24 @@ const Players = () => {
 
   return (
     <div>
-      <h1>List of Player</h1>
+      <h1>Add a new player</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="enter a player name"
+          value={playerName}
+          onChange={(e) => setPlayerName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="enter a country name"
+          value={countryName}
+          onChange={(e) => setCountryName(e.target.value)}
+        />
+        <button type="submit">Add Player</button>
+      </form>
 
+      <h1>List of Player</h1>
       {players?.map((player) => (
         <PlayerItem key={player.id} player={player} />
         // <div key={player.id}>
